c6f84dbe58bb6617ca69d53b66567d02fef25859
candice <candice2cc@gmail.com>
2016-08-31 10:51:41 +0800

    create snippet

---
+++ js/pattern.js
@@ -0,0 +1,15 @@
+/**
+ * js pattern util
+ * Created by candice on 16/8/31.
+ */
+;
+(function (pattern, window, document, undefined) {
+
+
+
+
+})(window.pattern = window.pattern || {}, window, document);
+
+
+
+
